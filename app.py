8dad1210de2890937b77bfdf2962ed83e0b24f78
Vallejo Bryan <bryan.vallejo@aalto.fi>
2023-03-10 11:16:26 +0200

    app

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask
 from keplergl import KeplerGl
+import geopandas as gpd
+
+#read
+data = gpd.read_file('data/azores_whales_records.gpkg', driver='GPKG')
+
+data['timestamp'] = data['timestamp'].astype(str)
 
 #map
 m = KeplerGl()
@@ -11,7 +17,7 @@ app = Flask(__name__)
 
 @app.route('/',  methods=['GET'])
 def index():
-    return m._repr_html_()
+    return m._repr_html_(data = {'wildlife':data})
     
 if __name__ == '__main__':
     app.run(debug=True, 
