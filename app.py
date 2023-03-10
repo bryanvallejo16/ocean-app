e30d9273b9557537b1c9201faa620982935347f2
Vallejo Bryan <bryan.vallejo@aalto.fi>
2023-03-10 10:30:05 +0200

    app

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from keplergl import KeplerGl
+
+#map
+m = KeplerGl()
+
+
+#app
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return m._repr_html_()
+    
+if __name__ == '__main__':
+    app.run(debug=True)
